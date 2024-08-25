b0da93345a0edfcea33cd2a4a0c2e8ddfb4f6b10
2TAP2B <steltner@pm.me>
2024-08-25 23:33:07 +0200

    playing around

--- src/config/navigationBar.ts
+++ src/config/navigationBar.ts
@@ -35,7 +35,7 @@ export const navigationBarData: NavData = {
 	logo: {
 		src: '/logo.svg',
 		alt: 'The tailwind astro theme',
-		text: 'Foxi.'
+		text: 'Steltner.cloud'
 	},
 	navItems: [
 		{ name: 'Home', link: '/' },
